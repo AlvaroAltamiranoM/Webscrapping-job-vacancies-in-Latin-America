28c9fd3314d14952ee59455f17b83bbddf28bedf
Alvaro Altamirano Montoya <48501231+AlvaroAltamiranoM@users.noreply.github.com>
2019-12-10 22:16:53 -0500

    Update infojobs.py

--- ingest/infojobs.py
+++ ingest/infojobs.py
@@ -15,7 +15,7 @@ import urllib
 from utils import *
 
 headers = {
-    'authority': 'www.empregos.com.br',
+    'authority': 'https://www.infojobs.com.br/',
     'cache-control': 'max-age=0',
     'dnt': '1',
     'upgrade-insecure-requests': '1',
@@ -27,9 +27,6 @@ headers = {
     'accept-encoding': 'gzip, deflate, br',
     'accept-language': 'en-US,en;q=0.9,es-MX;q=0.8,es;q=0.7'
 }
-
-country = ['bra']
-items_perpage = 20
 
 jobs = []
 URL_ofertas = []
@@ -47,8 +44,8 @@ items_perpage = 16
 jobs = []
 URL_ofertas = []
 #for pages in range(1,int((Ofertas_Activas/items_perpage)+2)):
-for pages in range(1,2):
-    URL = URL+'?Page='+format(pages)
+for pages in range(1,3):
+    URL = 'https://www.infojobs.com.br/empregos.aspx?Page='+format(pages)
     print(URL)
     #conducting a request of the stated URL above:
     page = requests.get(URL, headers=headers)
@@ -59,107 +56,75 @@ for pages in range(1,2):
         URL_ofertas.append(link['href'])
 
     #Fetching el contenido de cada oferta
-    details = []
-    for line in URL_ofertas:
-        detalle = {}
-        detalle["URL_ofertas"] = line
-        detalle["URL_ofertas"] = detalle["URL_ofertas"].replace("\t"," ")
-        detalle["URL_ofertas"] = urllib.parse.quote(detalle["URL_ofertas"], safe="%/:=&?~#+!$,;'@()*[]", encoding = 'utf-8')
-        #conducting a request of the stated URL above:
-        page = requests.get(detalle["URL_ofertas"], headers=headers, verify=False)
-        soup = BeautifulSoup(page.text, "html.parser")
-        #detalle["Sopa_de_queso"] = soup
-        tipos_box = ["advisor-card advisor-vacancy-content advisor-vacancy-summary"]
-        for t in tipos_box:
-            try:
-                box = soup.find(name="div", class_ = format(t)).find_all("li")
-            except:
-                pass
-        for element in box:
-            try:
-                a = element.find("span").text.replace(' +',' ').strip()
-                detalle[element.find("strong").text] = text_to_unicode(a)
-            except:
-                pass
-
+details = []
+for line in URL_ofertas:
+    detalle = {}
+    detalle["URL_ofertas"] = line
+    detalle["URL_ofertas"] = detalle["URL_ofertas"].replace("\t"," ")
+    detalle["URL_ofertas"] = urllib.parse.quote(detalle["URL_ofertas"], safe="%/:=&?~#+!$,;'@()*[]", encoding = 'utf-8')
+    #conducting a request of the stated URL above:
+    page = requests.get(detalle["URL_ofertas"], headers=headers, verify=False)
+    soup = BeautifulSoup(page.text, "html.parser")
+    #Variables del box derecho (principales variables)
+    tipos_box = ["advisor-card advisor-vacancy-content advisor-vacancy-summary"]
+    for t in tipos_box:
+        try:
+            box = soup.find(name="div", class_ = format(t)).find_all("li")
+        except:
+            pass
+    for element in box:
+        try:
+            a = element.find("span").text.replace(' +',' ').strip()
+            detalle[element.find("strong").text] = text_to_unicode(a)
+        except:
+            pass
+    try:
+        detalle["Empleador"] = soup.find(id = 'ctl00_phMasterPage_cVacancySummary_aCompany').text.strip()
+    except:
+        pass
+    try:
+        detalle["Educacion minima"] = soup.find(id = 'ctl00_phMasterPage_cVacancyManager_cVacancyRequeriments_liMinimunStudies').\
+        text.replace('Escolaridade Mínima: ',' ').strip()
+    except:
+        pass
+    try:
+        detalle["Idiomas"] = soup.find(id = 'ctl00_phMasterPage_cVacancyManager_cVacancyRequeriments_liLanguage').\
+        text.replace(' +',' ').strip()
+    except:
+        pass
+    #Variables del box izquierdo (descripcion de la vacantes)
+    tipos2 = ["descriptionItems"]
+    for t in tipos2:
+        try:
+            components = soup.find(name = "ol", class_ = format(t))
+            for element in components.find_all("li"):
+                try:
+                    a, b = element.text.split(":")
+                    detalle[text_to_unicode(a)] =text_to_unicode(b)
+                except:
+                    pass
+        except:
+            pass
         
-        #Nombre del puesto y local (departamento)
-        try:
-            detalle["Departamento"] = soup.select("div.cm-8.breadcrumb > ol > li:nth-of-type(2) > a")[0].get_text(strip=True)
-        except:
-            pass
-        try:
-            detalle["Empleador"] = soup.find(id = 'urlverofertas').text.strip()
-        except:
-            pass
-        tipos_box = ["box box_r", "boxWhite ocultar_mvl p30"]
-        for t in tipos_box:
-            try:
-                box = soup.find(name="section", class_ = format(t)).find_all("li")
-            except:
-                pass
-        for element in box:
-            try:
-                if country=='cl' or country=='ar' or country=='mx' or country=='co':
-                    a = element.find_all("p")[1].text.replace(' +',' ').strip()
-                    detalle[element.find(class_ = "fw_b fs15 mt10").text] = text_to_unicode(a)                  
-                else:
-                    a = element.find("p").text.replace(' +',' ').strip()
-                    detalle[element.find("h3").text] = text_to_unicode(a)
-            except:
-                pass
-        tipos2 = ["cm-12 box_i bWord", "cm-12 box_i", "boxWhite fl w_100 detail_of mb20 bWord"]
-        for t in tipos2:
-            try:
-                if country=='cl' or country=='ar' or country=='mx' or country=='co':
-                    a = soup.find(name="section", class_ =format(t)).find_all("li")[0].text.\
-                    replace('\nDescripción\r\n ', '').strip()
-                else:
-                    a = soup.find(name="div", class_ =format(t)).find_all("li")[0].text.\
-                    replace('\nDescripción\r\n ', '').strip()
-                detalle["descripcion"] = text_to_unicode(a)
-            except:
-                pass
-        for t in tipos2:
-            try:
-                if country=='cl' or country=='ar' or country=='mx' or country=='co':
-                    components = soup.find(name = "section",class_ = format(t))
-                else:
-                    components = soup.find(name = "div",class_ = format(t))
-                for element in components.find_all("li"):
-                    try:
-                        if element.find("h3") != None:
-                            pass
-                        else:
-                            a, b = element.text.split(":")
-                            detalle[text_to_unicode(a)] =text_to_unicode(b)
-                    except:
-                        pass
-            except:
-                pass
-        details.append(detalle)
+        UM DE DESCRICAO QUE PEGUE TODOS OS CAMPOS DESSA SECAO
+    details.append(detalle)
 
-    #Create and merge dataframes
-    df = pd.DataFrame(list(zip(jobs,URL_ofertas)), columns=["puesto2", "url_oferta"])
+#Creacion de dataframes y export a CSVs
+data = pd.DataFrame(details, columns = ["Area e especializacao profissional", "Numero de vagas", "Localidade", 
+                                        "Nivel hierarquico","Educacion minima", "Empleador", "Fecha de contratacion",
+                                        "Idiomas", "Jornada", "Título da vaga", "Salário", "Tipo de contrato", 
+                                        "URL_ofertas", "pais"])
 
-    data = pd.DataFrame(details, columns = ["Anos de experiencia", "Cantidad de vacantes", "Departamento", 
-                                            "Disponibilidad de cambio de residencia","Disponibilidad de viajar",
-                                            "Educacion minima", "Empleador", "Fecha de contratacion",
-                                            "Idiomas", "Jornada", "Licencias de conducir", "Puesto", "Salario",
-                                            "Tipo de contrato", "URL_ofertas", "descripcion"])
-
-    data.rename(columns = {'Anos de experiencia':'anios_de_experiencia', 'Cantidad de Vacantes':'cantidad_de_vacantes', 
-                           'Departamento':'departamento', 'Disponibilidad de Cambio de Residencia':'cambio_de_residencia',
-                           'Disponibilidad de Viajar':'disponibilidad_viajes', 'Educacion Minima':'educacion_minima', 
-                           'Empleador':'empresa', 'Fecha de Contratacion':'fecha_de_contratacion', 'Idiomas':'idiomas', 
-                           'Jornada':'jornada', 'Licencias de conducir':'licencia_conducir', 'Puesto':'puesto', 'Salario':'salario',
-                           'Tipo de contrato':'tipo_de_cotrato', 'URL_ofertas':'url_oferta', 'descripcion':'descripcion'}, inplace = True)
-    #data = data.drop(columns = ['puesto','Localización'])
-    data['puesto'] = df['puesto2']
-    data['date'] = date.today()
-    data['pais'] = country
-    data.to_csv(r'computrabajo_{0}_{1}.csv'.format(country, date.today()))
+data.rename(columns = {'Area e especializacao profissional': 'descripcion', 'Numero de vagas':'cantidad_de_vacantes', 
+                       'Nivel hierarquico': 'herarquia', 'Localidade':'departamento','Educacion Minima':'educacion_minima',
+                       'Empleador':'empresa', 'Idiomas':'idiomas','Jornada':'jornada', 'Título da vaga':'puesto', 
+                       'Salário':'salario', 'Tipo de contrato':'tipo_de_cotrato', 'URL_ofertas':'url_oferta'}, inplace = True)
+   
+data['date'] = date.today()
+country = ('bra')
+data['pais'] = country
+data.to_csv(r'infojobs_{0}_{1}.csv'.format(country, date.today()))
     
-
+    
     
     
