44f36976b5f8fae373c08664eeec2aaa9ba2a20d
Alvaro Altamirano Montoya <48501231+AlvaroAltamiranoM@users.noreply.github.com>
2019-12-22 07:57:12 -0500

    new variables, venezuela, etc.

--- ingest/computrabajo.py
+++ ingest/computrabajo.py
@@ -29,17 +29,28 @@ headers = {
     'accept-language': 'en-US,en;q=0.9,es-MX;q=0.8,es;q=0.7'
 }
 
-countries = ['ni', 'hn', 'cr', 'sv','pa', 'do', 'gt', 'co', 'mx', 'ar', 'pe', 'cl', 'ec', 'uy', 'py', 'bo']
+
+countries = ['ni', 'uy', 'py', 'bo', 'do', 'cr', 'hn', 'pa', 'gt', 'sv', 've', 'ec', 'cl', 'ar', 'pe', 'co', 'mx']
 items_perpage = 20
+baseline_date = '2019-12-22'
 
 for country in countries:
     jobs = []
     URL_ofertas = []
+    URL_ofertas2 = []
+    data_online = []
+    details = []
+    #Load history
+    history = pd.read_csv(r'computrabajo_{0}_{1}.csv'.format(country, baseline_date))
+    id_history = history.url_oferta
+
     #Identificar el # de ofertas activas en cada página-país en un momento dado
     if country == 'cr':
         URL = 'https://www.computrabajo.co.cr/ofertas-de-trabajo/'
     elif country == "sv":
         URL = 'https://www.sv.computrabajo.com/ofertas-de-trabajo/'
+    elif country == "ve":
+        URL = 'https://www.ve.computrabajo.com/ofertas-de-trabajo/'
     elif country == "cl":
         URL = 'https://www.computrabajo.cl/ofertas-de-trabajo/'        
     else:
@@ -58,11 +69,14 @@ for country in countries:
     Ofertas_Activas = soup.find(class_ = "breadtitle_mvl").text
     Ofertas_Activas = int(''.join(filter(str.isdigit, Ofertas_Activas)))
 
+    #for pages in range(12,13):
     for pages in range(1,int((Ofertas_Activas/items_perpage)+2)):
         if country == 'cr':
             URL = 'https://www.computrabajo.co.cr/ofertas-de-trabajo/?p='+format(pages)
         elif country == "sv":
             URL = 'https://www.sv.computrabajo.com/ofertas-de-trabajo/?p='+format(pages)
+        elif country == "ve":
+            URL = 'https://www.ve.computrabajo.com/ofertas-de-trabajo/?p='+format(pages)
         elif country == "cl":
             URL = 'https://www.computrabajo.cl/ofertas-de-trabajo/?p='+format(pages)
         else:
@@ -78,18 +92,22 @@ for country in countries:
         for t in tipos:
             for div in soup.find_all(name = 'div',attrs = {'class':format(t)}):
                 jobs.append(div.find(class_='js-o-link').text)
-        for link in soup.findAll(class_="js-o-link", href=True):
-            URL_ofertas.append(link['href'])
-
+                URL_ofertas.append(div.find(class_='js-o-link')['href'])
+                data_online.append(div.find(class_ = "dO").text.strip())
+                
+    # Query only new ofertas
+    ofertas_ = list(set(URL_ofertas) - set(id_history))
+    
     #Fetching el contenido de cada oferta
-    details = []
-
-    for line in URL_ofertas:
+    for line in ofertas_:
+    #for line in URL_ofertas:
         detalle = {}
         if country == 'cr':
             detalle["URL_ofertas"] = 'https://www.computrabajo.co.cr'+format(line)
         elif country == 'sv':
             detalle["URL_ofertas"] = 'https://www.sv.computrabajo.com'+format(line)
+        elif country == 've':
+            detalle["URL_ofertas"] = 'https://www.ve.computrabajo.com'+format(line)
         elif country == 'cl':
             detalle["URL_ofertas"] = 'https://www.computrabajo.cl'+format(line)
         else:
@@ -97,8 +115,12 @@ for country in countries:
             detalle["URL_ofertas"] = detalle["URL_ofertas"]+country+format(line)
         detalle["URL_ofertas"] = detalle["URL_ofertas"].replace("\t"," ")
         detalle["URL_ofertas"] = urllib.parse.quote(detalle["URL_ofertas"], safe="%/:=&?~#+!$,;'@()*[]", encoding = 'utf-8')
+        
+        detalle["URL_ofertas2"] = re.findall(r"[^.]ofertas[^.]*",detalle["URL_ofertas"])
+        detalle["URL_ofertas2"] = ', '.join(detalle["URL_ofertas2"])
+        
         #conducting a request of the stated URL above:
-        page = requests.get(detalle["URL_ofertas"], headers=headers, verify=False)
+        page = requests.get(detalle["URL_ofertas"], headers=headers)
         soup = BeautifulSoup(page.text, "html.parser")
         #detalle["Sopa_de_queso"] = soup
         try:
@@ -160,29 +182,88 @@ for country in countries:
                         pass
             except:
                 pass
+        #CAMPOS DE LA EMPRESA, RAMA DE ACTIVIDAD Y TAMANYO
+        detalle["URL_ofertas-empresa"] = detalle["URL_ofertas"]+'-empresa'
+        page = requests.get(detalle["URL_ofertas-empresa"], headers=headers)        
+        soup = BeautifulSoup(page.text, "html.parser")
+        try:
+            detalle["rama_de_actividad"] = soup.find(class_ = 'rComp mt10').find_all("span")[3].text.strip()
+        except:
+            pass
+        try:
+            detalle["tamanio_empresa"] = soup.find(class_ = 'rComp mt10').find_all("span")[5].text.strip()
+        except:
+            pass
+        
         details.append(detalle)
 
-    #Create and merge dataframes
-    df = pd.DataFrame(list(zip(jobs,URL_ofertas)), columns=["puesto2", "url_oferta"])
 
-    data = pd.DataFrame(details, columns = ["Anos de experiencia", "Cantidad de vacantes", "Departamento", 
+    #Create and merge dataframes
+
+    df = pd.DataFrame(list(zip(jobs, URL_ofertas, data_online)), 
+                      columns=["puesto2", "url_oferta", "fecha_online"])
+
+    if country=='co' or country=='cl' or country=='mx' or country=='pe':
+        data = pd.DataFrame(details, columns = ["Puesto","Anos de experiencia", "Cantidad de vacantes", "Departamento", 
                                             "Disponibilidad de cambio de residencia","Disponibilidad de viajar",
                                             "Educacion minima", "Empleador", "Fecha de contratacion",
-                                            "Idiomas", "Jornada", "Licencias de conducir", "Puesto", "Salario",
-                                            "Tipo de contrato", "URL_ofertas", "descripcion"])
-
-    data.rename(columns = {'Anos de experiencia':'anios_de_experiencia', 'Cantidad de Vacantes':'cantidad_de_vacantes', 
+                                            "Idiomas", "Jornada", "Licencias de conducir",  "Salario",
+                                            "Tipo de contrato", "URL_ofertas2", "descripcion", "rama_de_actividad",
+                                            "tamanio_empresa", "Edad", "Sexo", "Posiciones a cubrir", "Genero"])
+    elif country=='ar':
+        data = pd.DataFrame(details, columns = ["Puesto", "Anos de experiencia", "Departamento", 
+                                            "Disponibilidad de cambio de residencia","Disponibilidad de Viajar",
+                                            "Educacion minima", "Empleador",
+                                            "Idiomas", "Jornada", "Licencias de conducir",  "Salario",
+                                            "Tipo de contrato", "URL_ofertas2", "descripcion", "rama_de_actividad",
+                                            "tamanio_empresa", "Edad", "Sexo", "Posiciones a cubrir", "Genero"])
+    else:
+        data = pd.DataFrame(details, columns = ["Puesto", "Anos de experiencia", "Cantidad de Vacantes", "Departamento", 
+                                            "Disponibilidad de Cambio de Residencia","Disponibilidad de Viajar",
+                                            "Educacion Minima", "Empleador", "Fecha de Contratacion",
+                                            "Idiomas", "Jornada", "Licencias de conducir",  "Salario",
+                                            "Tipo de contrato", "URL_ofertas2", "descripcion", "rama_de_actividad",
+                                            "tamanio_empresa", "Edad", "Sexo", "Posiciones a cubrir", "Genero", "Localización"])
+    
+    if country=='co' or country=='cl' or country=='mx' or country=='pe':
+        data.rename(columns = {'Anos de experiencia':'anios_de_experiencia', 'Cantidad de vacantes':'cantidad_de_vacantes', 
+                           'Departamento':'departamento', 'Disponibilidad de cambio de residencia':'cambio_de_residencia',
+                           'Disponibilidad de viajar':'disponibilidad_viajes', 'Educacion minima':'educacion_minima', 
+                           'Empleador':'empresa', 'Fecha de contratacion':'fecha_de_contratacion', 'Idiomas':'idiomas', 
+                           'Jornada':'jornada', 'Licencias de conducir':'licencia_conducir', 'Puesto':'puesto', 'Salario':'salario',
+                           'Tipo de contrato':'tipo_de_contrato', 'URL_ofertas2':'url_oferta', 'descripcion':'descripcion',
+                           'Edad':'edad', 'Sexo': 'sexo', 'Genero':'genero'}, inplace = True)
+    
+    elif country=='ar':
+            data.rename(columns = {'Anos de experiencia':'anios_de_experiencia',  
+                           'Departamento':'departamento', 'Disponibilidad de cambio de residencia':'cambio_de_residencia',
+                           'Disponibilidad de Viajar':'disponibilidad_viajes', 'Educacion minima':'educacion_minima', 
+                           'Empleador':'empresa', 'Idiomas':'idiomas', 
+                           'Jornada':'jornada', 'Licencias de conducir':'licencia_conducir', 'Puesto':'puesto', 'Salario':'salario',
+                           'Tipo de contrato':'tipo_de_contrato', 'URL_ofertas2':'url_oferta', 'descripcion':'descripcion',
+                           'Edad':'edad', 'Sexo': 'sexo', 'Genero':'genero'}, inplace = True)
+    else:
+        data.rename(columns = {'Anos de experiencia':'anios_de_experiencia', 'Cantidad de Vacantes':'cantidad_de_vacantes', 
                            'Departamento':'departamento', 'Disponibilidad de Cambio de Residencia':'cambio_de_residencia',
                            'Disponibilidad de Viajar':'disponibilidad_viajes', 'Educacion Minima':'educacion_minima', 
                            'Empleador':'empresa', 'Fecha de Contratacion':'fecha_de_contratacion', 'Idiomas':'idiomas', 
                            'Jornada':'jornada', 'Licencias de conducir':'licencia_conducir', 'Puesto':'puesto', 'Salario':'salario',
-                           'Tipo de contrato':'tipo_de_contrato', 'URL_ofertas':'url_oferta', 'descripcion':'descripcion'}, inplace = True)
-    #data = data.drop(columns = ['puesto','Localización'])
-    data['puesto'] = df['puesto2']
+                           'Tipo de contrato':'tipo_de_contrato', 'URL_ofertas2':'url_oferta', 'descripcion':'descripcion',
+                           "Localización":"local_detalle", 'Edad':'edad', 'Sexo': 'sexo', 'Genero':'genero'}, inplace = True)
+
+    if country=='mx' or country=='co' or country=='cl' or country=='ar':
+        data['puesto'] = df['puesto2'] 
+        
     data['date'] = date.today()
     data['pais'] = country
-    data.to_csv(r'computrabajo_{0}_{1}.csv'.format(country, date.today()))
     
-
+    data = data.merge(df, how="left", on="url_oferta", indicator=True)
+    data = data.drop(columns = ['puesto2'])
+      
+    #data.to_csv(r'computrabajo_{0}_{1}.csv'.format(country, date.today()))  
+    
+    history.append(data, sort = True, ignore_index=True).to_csv(r'computrabajo_{0}_{1}.csv'.format(country, date.today()))
     
     
+    
+   
