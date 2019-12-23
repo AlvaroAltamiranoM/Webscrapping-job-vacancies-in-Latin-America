8ab41bdc6d4f2a5e464afa49cbb0f7739ea31040
Alvaro Altamirano Montoya <48501231+AlvaroAltamiranoM@users.noreply.github.com>
2019-12-23 11:45:11 -0500

    Update infojobs.py

--- ingest/infojobs.py
+++ ingest/infojobs.py
@@ -28,8 +28,8 @@ headers = {
     'accept-language': 'en-US,en;q=0.9,es-MX;q=0.8,es;q=0.7'
 }
 
-jobs = []
 URL_ofertas = []
+country = ('bra')
 #Identificar el # de ofertas activas en cada página en un momento dado
 URL = 'https://www.infojobs.com.br/empregos.aspx'
 #conducting a request of the stated URL above:
@@ -41,10 +41,16 @@ Ofertas_Activas = int(''.join(filter(str.isdigit, Ofertas_Activas)))
 print(Ofertas_Activas)
 items_perpage = 16
 
-jobs = []
+data_online = []
 URL_ofertas = []
+details = []
+
+baseline_date = '2019-12-14'
+history = pd.read_csv(r'infojobs_{0}_{1}.csv'.format(country, baseline_date))
+id_history = history.url_oferta
+
 for pages in range(1,int((Ofertas_Activas/items_perpage)+2)):
-#for pages in range(16,19):
+#for pages in range(16,17):
     URL = 'https://www.infojobs.com.br/empregos.aspx?Page='+format(pages)
     print(URL)
     #conducting a request of the stated URL above:
@@ -54,12 +60,17 @@ for pages in range(1,int((Ofertas_Activas/items_perpage)+2)):
     #Extracting job urls
     for link in soup.findAll(class_="vagaTitle js_vacancyTitle", href=True):
         URL_ofertas.append(link['href'])
+    for p in soup.find_all(name="p", attrs={"class": "location2"}):
+        data_online.append(p.find(name="span").text.strip())
 
-    #Fetching el contenido de cada oferta
-details = []
-for line in URL_ofertas:
+# Query only new ofertas
+ofertas_ = list(set(URL_ofertas) - set(id_history))
+
+#Fetching el contenido de cada oferta
+for line in ofertas_:
+#for line in URL_ofertas:
     detalle = {}
-    detalle["URL_ofertas"] = line
+    detalle["URL_ofertas"] = line    
     detalle["URL_ofertas"] = detalle["URL_ofertas"].replace("\t"," ")
     detalle["URL_ofertas"] = urllib.parse.quote(detalle["URL_ofertas"], safe="%/:=&?~#+!$,;'@()*[]", encoding = 'utf-8')
     #conducting a request of the stated URL above:
@@ -117,46 +128,36 @@ for line in URL_ofertas:
         except:
             pass
     try:
-        detalle["descripcion"] = soup.select("#ctl00_phMasterPage_divVacancy > div.advisor-vacancy-content > ol:nth-child(2)")[0].\
+        detalle["descripcion"] = soup.select("#ctl00_phMasterPage_divVacancy > div.advisor-vacancy-content > ol")[0].\
         get_text(strip=True).replace('Área e especialização profissional:', '')
     except:
-        pass       
+        pass    
 
-#Para personas con discapcidad
-# =============================================================================
-#     tipos3 = ["ctl00_phMasterPage_cVacancyManager_cVacancyDeficiencies_liDeficiencies"]
-#     for t in tipos3:
-#         try:
-#             components_discapacidad = soup.find(name = "ol", id = format(t))
-#             for element in components_discapacidad.find_all("li"):
-#                 try:
-#                     a, b = element.text.split(":")
-#                     detalle[text_to_unicode(a)] =text_to_unicode(b)
-#                 except:
-#                     pass
-#         except:
-#             pass
-# =============================================================================
-        
     details.append(detalle)
 
 #Creacion de dataframes y export a CSVs
+df = pd.DataFrame(list(zip(data_online, URL_ofertas)),
+        columns=["fecha_online", "url_oferta"])
+
 data = pd.DataFrame(details, columns = ["Area e especializacao profissional", "Numero de vagas", "Localidade", 
                                         "Nivel hierarquico","Educacion minima", "Empleador", "Idiomas", 
                                         "Jornada", "Título da vaga", "Salário", "Tipo de contrato", 
                                         "URL_ofertas", "pais", "Licencias de conducir", 'Vehículo', 'descripcion'])
-
+    
 data.rename(columns = {'Area e especializacao profissional': 'especialidad', 'Numero de vagas':'cantidad_de_vacantes', 
                        'Nivel hierarquico': 'jerarquia', 'Localidade':'departamento','Educacion Minima':'educacion_minima',
                        'Empleador':'empresa', 'Idiomas':'idiomas','Jornada':'jornada', 'Título da vaga':'puesto', 
                        'Salário':'salario', 'Tipo de contrato':'tipo_de_contrato', 'URL_ofertas':'url_oferta',
                        'Licencias de conducir':'licencia_conducir', 'Vehículo': 'vehiculo', 'descripcion':'descripcion'}, inplace = True)
 
+data = df.merge(data, how="left", on="url_oferta", indicator=True)
+
 data['date'] = date.today()
-country = ('bra')
 data['pais'] = country
-data.to_csv(r'infojobs_{0}_{1}.csv'.format(country, date.today()))
+#data.to_csv(r'infojobs_{0}_{1}.csv'.format(country, date.today()))
+
+history.append(data).to_csv(r'infojobs_{0}_{1}.csv'.format(country, date.today()))
+
     
     
-    
-    
+   
