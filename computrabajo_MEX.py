dc00a354092e8ceb2fe5fc951c8dea214e65e763
Alvaro Altamirano Montoya <48501231+AlvaroAltamiranoM@users.noreply.github.com>
2020-02-17 13:58:50 -0500

    Added descripcion, var names

--- computrabajo_MEX.py
+++ computrabajo_MEX.py
@@ -28,17 +28,19 @@ headers = {
 }
 
 
+
+
 items_perpage = 20
-baseline_date = '2019-12-22'
+baseline_date = '2020-02-12'
 
 jobs = []
-URL_ofertas = []
-URL_ofertas2 = []
+url_ofertas = []
+url_ofertas2 = []
 data_online = []
 details = []
 #Load history
-#history = pd.read_csv(r'computrabajo_{0}.csv'.format(baseline_date))
-#id_history = history.url_oferta
+history = pd.read_csv(r'computrabajo_{0}.csv'.format(baseline_date))
+id_history = history.url_oferta
 
 #Identificar el # de ofertas activas en cada página-país en un momento dado
 URL = 'https://www.computrabajo.com.mx/ofertas-de-trabajo/'
@@ -64,45 +66,44 @@ for pages in range(1,int((Ofertas_Activas/items_perpage)+2)):
     for t in tipos:
         for div in soup.find_all(name = 'div',attrs = {'class':format(t)}):
             jobs.append(div.find(class_='js-o-link').text)
-            URL_ofertas.append(div.find(class_='js-o-link')['href'])
+            url_ofertas.append(div.find(class_='js-o-link')['href'])
             data_online.append(div.find(class_ = "dO").text.strip())
             
 # Query only new ofertas
-#ofertas_ = list(set(URL_ofertas) - set(id_history))
+ofertas_ = list(set(url_ofertas) - set(id_history))
 
 #Fetching el contenido de cada oferta
-#for line in ofertas_:
-#for line in URL_ofertas:
+detalle = {}
+for line in ofertas_:
+#for line in url_ofertas:
     detalle = {}
-for line in URL_ofertas:
-    detalle = {}
-    detalle["URL_ofertas"] = 'https://www.computrabajo.com.mx/ofertas-de-trabajo/'
-    detalle["URL_ofertas"] = detalle["URL_ofertas"]+format(line)
-    detalle["URL_ofertas"] = detalle["URL_ofertas"].replace("\t"," ")
-    detalle["URL_ofertas"] = urllib.parse.quote(detalle["URL_ofertas"], safe="%/:=&?~#+!$,;'@()*[]", encoding = 'utf-8')
+    detalle["url_ofertas"] = 'https://www.computrabajo.com.mx'
+    detalle["url_ofertas"] = detalle["url_ofertas"]+format(line)
+    detalle["url_ofertas"] = detalle["url_ofertas"].replace("\t"," ")
+    detalle["url_ofertas"] = urllib.parse.quote(detalle["url_ofertas"], safe="%/:=&?~#+!$,;'@()*[]", encoding = 'utf-8')
     
-    detalle["URL_ofertas2"] = re.findall(r"[^.]ofertas[^.]*",detalle["URL_ofertas"])
-    detalle["URL_ofertas2"] = ', '.join(detalle["URL_ofertas2"])
-    
+    detalle["url_ofertas2"] = re.findall(r"[^.]ofertas[^.]*",detalle["url_ofertas"])
+    detalle["url_ofertas2"] = ', '.join(detalle["url_ofertas2"])
     #conducting a request of the stated URL above:
-    page = requests.get(detalle["URL_ofertas"], headers=headers)
+    print(detalle["url_ofertas"])
+    page = requests.get(detalle["url_ofertas"], headers=headers)
     soup = BeautifulSoup(page.text, "html.parser")
     #detalle["Sopa_de_queso"] = soup
     try:
         aux = soup.find(class_="cm-8 box detalle_oferta box_image").find("h1").text
-        detalle["Puesto"] = re.sub("\r|\n|\s\s+",'',aux)
+        detalle["puesto"] = re.sub("\r|\n|\s\s+",'',aux)
     except:
         pass
     #Nombre del puesto y local (departamento)
     try:
-        detalle["Departamento"] = soup.select("div.cm-8.breadcrumb > ol > li:nth-of-type(2) > a")[0].get_text(strip=True)
+        detalle["localidad"] = soup.select("div.cm-8.breadcrumb > ol > li:nth-of-type(2) > a")[0].get_text(strip=True)
     except:
         pass
     try:
-        detalle["Empleador"] = soup.find(id = 'urlverofertas').text.strip()
+        detalle["empresa"] = soup.find(id = 'urlverofertas').text.strip()
     except:
         pass
-    tipos_box = ["box box_r", "boxWhite ocultar_mvl p30"]
+    tipos_box = ["boxWhite ocultar_mvl p30"]
     for t in tipos_box:
         try:
             box = soup.find(name="section", class_ = format(t)).find_all("li")
@@ -119,6 +120,7 @@ for line in URL_ofertas:
         try:
             a = soup.find(name="section", class_ =format(t)).find_all("li")[0].text.\
             replace('\nDescripción\r\n ', '').strip()
+            detalle["descripcion"] = text_to_unicode(a)
         except:
             pass
     for t in tipos2:
@@ -136,8 +138,8 @@ for line in URL_ofertas:
         except:
             pass
     #CAMPOS DE LA EMPRESA, RAMA DE ACTIVIDAD Y TAMANYO
-    detalle["URL_ofertas-empresa"] = detalle["URL_ofertas"]+'-empresa'
-    page = requests.get(detalle["URL_ofertas-empresa"], headers=headers)        
+    detalle["url_ofertas-empresa"] = detalle["url_ofertas"]+'-empresa'
+    page = requests.get(detalle["url_ofertas-empresa"], headers=headers)        
     soup = BeautifulSoup(page.text, "html.parser")
     try:
         detalle["rama_de_actividad"] = soup.find(class_ = 'rComp mt10').find_all("span")[3].text.strip()
@@ -153,21 +155,21 @@ for line in URL_ofertas:
 
 #Create and merge dataframes
 
-df = pd.DataFrame(list(zip(jobs, URL_ofertas, data_online)), 
+df = pd.DataFrame(list(zip(jobs, url_ofertas, data_online)), 
                   columns=["puesto2", "url_oferta", "fecha_online"])
 
-data = pd.DataFrame(details, columns = ["Puesto","Anos de experiencia", "Cantidad de vacantes", "Departamento", 
+data = pd.DataFrame(details, columns = ["puesto","Anos de experiencia", "Cantidad de vacantes", "localidad", 
                                         "Disponibilidad de cambio de residencia","Disponibilidad de viajar",
-                                        "Educacion minima", "Empleador", "Fecha de contratacion",
+                                        "Educacion minima", "empresa", "Fecha de contratacion",
                                         "Idiomas", "Jornada", "Licencias de conducir",  "Salario",
-                                        "Tipo de contrato", "URL_ofertas2", "descripcion", "rama_de_actividad",
+                                        "Tipo de contrato", "url_ofertas2", "descripcion", "rama_de_actividad",
                                         "tamanio_empresa", "Edad", "Sexo", "Posiciones a cubrir", "Genero"])
+#Renames    
 data.rename(columns = {'Anos de experiencia':'anios_de_experiencia', 'Cantidad de vacantes':'cantidad_de_vacantes', 
-                       'Departamento':'departamento', 'Disponibilidad de cambio de residencia':'cambio_de_residencia',
-                       'Disponibilidad de viajar':'disponibilidad_viajes', 'Educacion minima':'educacion_minima', 
-                       'Empleador':'empresa', 'Fecha de contratacion':'fecha_de_contratacion', 'Idiomas':'idiomas', 
-                       'Jornada':'jornada', 'Licencias de conducir':'licencia_conducir', 'Puesto':'puesto', 'Salario':'salario',
-                       'Tipo de contrato':'tipo_de_contrato', 'URL_ofertas2':'url_oferta', 'descripcion':'descripcion',
+                       'Disponibilidad de cambio de residencia':'cambio_de_residencia', 'Disponibilidad de viajar':'disponibilidad_viajes', 'Educacion minima':'educacion_minima', 
+                       'Fecha de contratacion':'fecha_de_contratacion', 'Idiomas':'idiomas', 
+                       'Jornada':'jornada', 'Licencias de conducir':'licencia_conducir', 'Salario':'salario',
+                       'Tipo de contrato':'tipo_de_contrato', 'url_ofertas2':'url_oferta',
                        'Edad':'edad', 'Sexo': 'sexo', 'Genero':'genero'}, inplace = True)
 
 data['puesto'] = df['puesto2'] 
@@ -177,6 +179,6 @@ data['date'] = date.today()
 data = data.merge(df, how="left", on="url_oferta", indicator=True)
 data = data.drop(columns = ['puesto2'])
   
-data.to_csv(r'computrabajo_{0}.csv'.format(date.today()))  
+#data.to_csv(r'computrabajo_{0}.csv'.format(date.today()))  
 
-#history.append(data, sort = True, ignore_index=True).to_csv(r'computrabajo_{0}_{1}.csv'.format(country, date.today()))
+history.append(data, sort = True, ignore_index=True).to_csv(r'computrabajo_{0}.csv'.format(date.today()))
