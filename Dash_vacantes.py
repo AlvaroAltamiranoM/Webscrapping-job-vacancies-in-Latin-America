e6e21f85564d72946d41780bc78b7d08440a53b7
Alvaro Altamirano Montoya <48501231+AlvaroAltamiranoM@users.noreply.github.com>
2020-04-08 11:59:12 -0400

    Update Dash_vacantes.py

--- Dash_vacantes.py
+++ Dash_vacantes.py
@@ -27,6 +27,9 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 #Data
 Data_test = pd.read_csv(r'data_dash.csv', encoding='utf-8')
 
+Data_test['date'][0:500]='enero'
+Data_test['date'][500:1000]='febrero'
+
 Data_test['salarios'] = Data_test['salario'].str.extract('(\d*\.?\d+)')
 Data_test['salarios'] = Data_test['salarios'].str.replace('.', '').astype(float)
 
@@ -38,59 +41,34 @@ Data_test['edad_max'] = Data_test['edad'].astype(str).str[-2:]
 Data_test['edad_max'] = pd.to_numeric(Data_test['edad_max'], errors='coerce')
 Data_test['edad'] = Data_test[['edad_min','edad_max']].mean(axis=1)
 
-table = pd.pivot_table(Data_test, 
-                     index = 'localidad',
-                     values=['salarios', 'anios_de_experiencia', 'url_oferta', 'edad', 'educacion_minima', 'tamanio_empresa'],
+table = Data_test.groupby(['date','localidad']).size().reset_index().rename(columns={0:'conteo'})
+
+table2 = pd.pivot_table(Data_test, 
+                     index = ['localidad'],
+                     values=['salarios', 'anios_de_experiencia', 'url_oferta', 'edad', 'educacion_minima', 'rama_de_actividad'],
                      aggfunc={'salarios': np.median, 'anios_de_experiencia': np.mean, 'url_oferta': pd.Series.nunique,
-                              'edad': np.mean, 'educacion_minima': pd.Series.mode, 'tamanio_empresa': pd.Series.mode},
+                              'edad': np.mean, 'educacion_minima': pd.Series.mode, 'rama_de_actividad': pd.Series.mode},
                      dropna=True)
 
-anuncios_estado = Data_test.groupby(['localidad'])['url_oferta'].count()
-anuncios_estado = anuncios_estado.nlargest(10).sort_values(ascending=False)
+anuncios_empresa = Data_test.groupby(['empresa','localidad', 'date']).size().reset_index().rename(columns={0:'conteo'})
 
-empresas = Data_test['empresa'].value_counts()
-empresas = empresas.nlargest(30).sort_values(ascending=False)
-
-table['Estados']= table.index
-table = table.nlargest(10, 'url_oferta').sort_values('url_oferta', ascending=False)
-
-table.rename(columns = {'anios_de_experiencia': 'Años de experiencia (en media)', 'edad':'Edad requerida (media)', 
-                       'salarios': 'Mediana salarial','tamanio_empresa':'Tamaño de empresas (moda)',
+table2.rename(columns = {'anios_de_experiencia': 'Años de experiencia (en media)', 'edad':'Edad requerida (media)', 
+                       'salarios': 'Mediana salarial','rama_de_actividad':'Rama de actividad (moda)',
                        'url_oferta':'Total de anuncios'}, inplace = True)
 
-table['Años de experiencia (en media)']= table['Años de experiencia (en media)'].round(1)
-table['Edad requerida (media)']= table['Edad requerida (media)'].round(0)
+table2['Años de experiencia (en media)']= table2['Años de experiencia (en media)'].round(1)
+table2['Edad requerida (media)']= table2['Edad requerida (media)'].round(0)
+table2['Mediana salarial']= table2['Mediana salarial'].round(0)
 
-table = table.reindex(['Estados','Total de anuncios', 'Mediana salarial','Años de experiencia (en media)', 
-                       'Edad requerida (media)','Tamaño de empresas (moda)'], axis=1)
+table_table = table2.reindex(['Estados','Total de anuncios', 'Mediana salarial','Años de experiencia (en media)', 
+                       'Edad requerida (media)','Rama de actividad (moda)'], axis=1).sort_values(by='Total de anuncios',ascending=False)
+table_table['Estados'] = table_table.index
 
-words = empresas.index
+words = anuncios_empresa.empresa
 lenth = len(words)
 colors = [plotly.colors.DEFAULT_PLOTLY_COLORS[random.randrange(1, 10)] for i in range(30)]
-weights = empresas.values
+weights = anuncios_empresa.conteo
 
-data = go.Scatter(x=[random.random() for i in range(30)],
-                 y=random.choices(range(30), k=30),
-                 mode='text',
-                 text=words,
-                 marker={'opacity': 0.3},
-                 textfont={'size': weights,
-                           'color': colors})
-layout = go.Layout({'xaxis': {'showgrid': False, 'showticklabels': False, 'zeroline': False},
-                    'yaxis': {'showgrid': False, 'showticklabels': False, 'zeroline': False}})
-
-fig = go.Figure(data=[data], layout=layout)
-fig.update_layout(
-    title={
-        'text': "Principales empresas",
-        'y':0.9,
-        'x':0.5,
-        'xanchor': 'center',
-        'yanchor': 'top'})
-
-#for estado in anuncios_estado['localidad']:
- #   anuncios_estado = anuncios_estado['url_oferta'].nlargest(10).sort_values(ascending=False)
-    
 app.layout = html.Div(children=[
     html.H1(children='México',
     style={'textAlign': 'center'
@@ -103,31 +81,29 @@ app.layout = html.Div(children=[
     html.Div([
         dcc.Dropdown(
             id='xaxis-column',
-            options=[{'label': i, 'value': i} for i in table.Estados],
-            placeholder='Seleccione un Estado'),
+            options=[{'label': i, 'value': i} for i in table.localidad.unique()],
+            placeholder='Seleccione un Estado',
+            value = ['Ciudad de México DF','Jalisco', 'Puebla'],
+            multi =True),
             ],
             style={'width': '48%', 'display': 'inline-block',
                     'marginTop': '1em','marginBottom': '1em'}),
     html.Div([
         dcc.Dropdown(
             id='yaxis-column',
-            options=[{'label': i, 'value': i} for i in Data_test.date.unique()],
-            placeholder='Seleccione un Estado'),
+            options=[{'label': i, 'value': i} for i in table.date.unique()],
+            placeholder='Seleccione un mes',
+            value = ['enero'],
+            multi =True),
             ],
              style={'width': '48%', 'display': 'inline-block',
                     'marginTop': '1em','marginBottom': '1em'}),
     
     html.Div([
-    dcc.Graph(
-        id='graph1',
-        figure={
-            'data': [{'x': anuncios_estado.index,  'y': anuncios_estado, 'type': 'bar', 'name': 'México'},
-             ],
-            'layout': {'title': 'Número de vacantes por Portal'}
-            },
-            style={'width': '48%', 'display': 'inline-block'}),
+    dcc.Graph(id='graph1',
+              style={'width': '48%', 'display': 'inline-block'}),
         
-        dcc.Graph(id='graph2',figure = fig,
+        dcc.Graph(id='graph2',
                   style={'width': '48%', 'display': 'inline-block'})
         ] ),
 
@@ -138,14 +114,16 @@ app.layout = html.Div(children=[
     dash_table.DataTable(
             style_data={
         'whiteSpace': 'normal',
-        'height': 'auto',
+        'height': 'auto'
         },
-    data=table.to_dict('records'),
+    data=table_table.to_dict('records'),
         sort_action="native",
         filter_action='native',
         sort_mode="multi",
-        columns=[{'id': c, 'name': c} for c in table.columns],
-        style_table={'overflowX': 'scroll'},
+        columns=[{'id': c, 'name': c} for c in table_table.columns],
+        style_table={'overflowX': 'scroll',
+                     'maxHeight':'400px',
+                     'overflowY':'scroll'},
         style_as_list_view=True,
         style_cell={'padding': '5px','textAlign': 'center',
                     },
@@ -154,7 +132,7 @@ app.layout = html.Div(children=[
             'backgroundColor': 'rgb(230, 230, 230)',
             },
             style_cell_conditional=[
-                {'if': {'column_id': 'Estados'},
+                {'if': {'column_id': 'Ciudades'},
                     'textAlign': 'left',
                     'width': '170px'},
                  {'if': {'column_id': 'Años de experiencia (en media)'},
@@ -168,8 +146,58 @@ app.layout = html.Div(children=[
     ]
     )
     ])
-    
 
+@app.callback(
+    Output('graph1', 'figure'),
+    [Input('xaxis-column', 'value'),
+     Input('yaxis-column', 'value')])
+
+def update_graph(xaxis_column_name, year_value):
+    #table_g = table.loc[table['localidad'].isin(xaxis_column_name)]
+    if len(xaxis_column_name) > 0 and len(year_value) > 0:
+        table_g = table.loc[table['localidad'].isin(xaxis_column_name) & table['date'].\
+                            isin(year_value)].sort_values(by='conteo', ascending=False)
     
+    return {
+            'data': [dict(
+            x=table_g['localidad'],
+            y=table_g['conteo'],
+            type='bar',
+        )],
+        'layout': dict(
+            xaxis={
+                'title': 'Estado',
+                'yanchor': 'top'},
+            yaxis={
+                'title': 'Número'},
+            title={
+                'text': 'Vacantes en: <br>{}'.format(', '.join(xaxis_column_name)),
+                'xanchor': 'auto',
+                'y': 0.9})
+        }
+
+@app.callback(
+    Output('graph2', 'figure'),
+    [Input('xaxis-column', 'value'),
+     Input('yaxis-column', 'value')])
+        
+def update_graph2(xaxis_column_name, year_value):
+    table3 = anuncios_empresa[anuncios_empresa['localidad'].isin(xaxis_column_name)]
+    data = go.Scatter(
+    x=[random.random() for i in range(30)],
+    y=random.choices(range(30), k=30),
+    mode='text',
+    text=table3['empresa'][~(table3['empresa'].str.len() <4)].unique(),
+    marker={'opacity': 0.3},
+    textfont={'size': table3['conteo']*5,
+   'color': colors})
+ 
+    return {'data': [data],'layout' : go.Layout(xaxis={'showgrid': False, 'showticklabels': False, 'zeroline': False},
+                                                yaxis={'showgrid': False, 'showticklabels': False, 'zeroline': False},
+                                                title={'text': "Principales empresas",'y':0.9,'x':0.5, 'xanchor': 'center','yanchor': 'top'})
+            }
+
+
 if __name__ == '__main__':
-    app.run_server(debug=False) 
+    app.run_server(debug=False)
+
